f156baf20a07e0dab6313f65559de867ca5d3679
elquemejorlohace5 <32984470+elquemejorlohace5@users.noreply.github.com>
2017-10-21 11:19:34 -0500

    creacion index1
    rama2

---
+++ Nueva carpeta/index1.php
@@ -0,0 +1,58 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Content-Language" content="English" />
+<meta name="Distribution" content="Global" />
+<meta name="Author" content="Luka Cvrk (luka@solucija.com)" />
+<meta name="Robots" content="index,follow" />
+<meta name="Description" content="Internet Jobs" />
+<meta name="Keywords" content="internet, jobs, business, online, marketing" />
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+<title>Internet Jobs</title>
+</head>
+<body>
+<div id="header">
+	<table width="100%" cellpadding="0" cellspacing="0" height="100%">
+    	<tr>
+        	<td width="270">
+				<div id="logotip">
+                	<font size="+2">CdeT</font>
+                    <font size="-1" color="#000000">MIC</font>
+                </div>
+            </td>
+            <td align="right" valign="bottom">
+            	Josele - <a href="#">Salir</a>
+                <br />
+                <font color="#333333"><?php echo strftime("%A %d de %B del %Y");?></font>
+            </td>
+        </tr>
+    </table>
+</div>
+<div class="content">
+  <div id="articles">
+  <div id="left">
+      <?php $_SESSION["permission"] = "planner"; CreateLeftMenu(); ?>
+  </div>
+    <div id="right">
+      <h2>Lorem Ipsum</h2>
+      <p><img src="images/router.jpg" alt="" width="150" height="100" class="right" />Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent lupt...</p>
+      <p class="date"><img src="images/more.gif" alt="" /> <a href="#">Read more</a></p>
+      <h2>Lorem Ipsum</h2>
+      <p><img src="images/router.jpg" alt="" width="150" height="100" class="left" />Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent lupt...</p>
+      <p class="date"><img src="images/more.gif" alt="" /> <a href="#">Read more</a></p>
+      <h2>Lorem Ipsum</h2>
+      <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent lupt...</p>
+      <p class="date"><img src="images/more.gif" alt="" /> <a href="#">Read more</a></p>
+    </div>    
+  </div>
+  <div id="links">
+  </div>
+</div>
+<div id="footer">
+  <p style="float: right; text-align: right;"><a href="#">About Us</a> <a href="#">Links</a> <a href="#">Contact Us</a><br />
+    (c) 2004-2006 All rights reserved. Company Name.com.</p>
+</div>
+</body>
+</html>
